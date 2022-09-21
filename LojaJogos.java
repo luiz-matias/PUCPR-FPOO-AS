4e07ef66762e17853dcdb8726c04a4e17ff602a6
Luiz Matias <luiz.matias1999@gmail.com>
2022-09-21 04:37:19 -0300

    Created main class (TODO: JOptionPane for menu interaction)

---
+++ LojaJogos.java
@@ -0,0 +1,32 @@
+package LojaJogos;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class LojaJogos {
+    private ArrayList<Produto> produtos;
+
+    public LojaJogos() {
+        this.produtos = new ArrayList<Produto>();
+    }
+
+    /*
+    TODO Implementar Sistema de UI para interagir com os objetos (JOptionPane)
+        - 1. Inserir
+        - 2. Exibir
+        - 3. Limpar
+        - 4. Gravar em arquivo
+        - 5. Recuperar de arquivo
+        - 9. Sair
+     */
+
+    public static void main(String[] args) {
+
+        LojaJogos loja = new LojaJogos();
+        //TODO Iniciar menu aqui
+
+    }
+
+}
